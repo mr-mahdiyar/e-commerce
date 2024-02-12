51bed1a849bf0a1d81c3213e13822aa583c6c086
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-12 08:37:41 +0330

    dynamic categories were added to Navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,20 @@ import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setSidebarOn, getSidebarStatus } from "../../store/SidebarSlice";
-
+import { getAllCategories } from "../../store/CategorySlice";
 const Navbar = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   //const sidebarStatus = useSelector(getSidebarStatus)
-
+  const categories = useSelector(getAllCategories());
   return (
     <nav className="navbar">
       <div className="navbar-cnt flex align-center">
         <div className="brand-and-toggler flex align-center">
-          <button type="button" className="sidebar-show-btn text-white" onClick={() => dispatch(setSidebarOn())}>
+          <button
+            type="button"
+            className="sidebar-show-btn text-white"
+            onClick={() => dispatch(setSidebarOn())}
+          >
             <i className="fas fa-bars"></i>
           </button>
           <Link to="/" className="navbar-brand flex align-center">
@@ -43,12 +46,13 @@ const Navbar = () => {
           </div>
 
           <ul className="navbar-nav flex align-center fs-12 fw-4 font-manrope">
-            <li className="nav-item no-wrap">
-              <Link
-                to=""
-                className="nav-link text-capitalize"
-              >category here</Link>
-            </li>
+            {categories.slice(0, 8).map((category, idx) => (
+              <li className="nav-item no-wrap" key={idx}>
+                <Link to={`category/${category}`} className="nav-link text-capitalize">
+                  {category}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
