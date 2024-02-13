19a055a84cbde5d1f217a569263fb444389c9f80
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-13 14:29:03 +0330

    producst reducer was added to store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SidebarReducer from "./SidebarSlice";
 import CategoryReducer from "./CategorySlice";
-
+import ProductsSlice from "./ProductsSlice";
 const store = configureStore({
     reducer: {
         sidebar: SidebarReducer,
-        category: CategoryReducer
+        category: CategoryReducer,
+        producst: ProductsSlice
     }
 })
 
