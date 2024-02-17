c19a16feefaeb04fc7eca92651e4a154a1147178
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-17 06:26:00 +0330

    ProductSinglPage files were added.

---
+++ src/pages/ProductSinglePage/ProductSinglePage.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useState } from "react";
+import "./ProductSinglePage.scss";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchAsyncSingleProduct,
+  getProductSingle,
+  getSingleProductStatus,
+} from "../../app/productSlice";
+import { STATUS } from "../../utils/status";
+import { formatPrice } from "../../utils/helpers";
+import { Loader } from "../../";
+
+const ProductSinglePage = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAsyncSingleProduct(id));
+  }, [dispatch]);
+
+  const product = useSelector(getProductSingle);
+  const singleProductStatus = useSelector(getSingleProductStatus);
+
+  const [quantity, setQuantity] = useState(1);
+
+  let discountedPrice =
+    product?.price - product?.price * (product?.discountPercentage / 100);
+
+  if (singleProductStatus === STATUS.LOADING) {
+    return <Loader />;
+  }
+
+  const increaseQty = () => {
+    setQuantity((prevState) => {
+      if (prevState + 1 > product?.stock) {
+        return product?.stock;
+      }
+      return prevState + 1;
+    });
+  };
+  const decreaseQty = () => {
+    setQuantity((prevState) => {
+      if (prevState - 1 < 1) {
+        return 1;
+      }
+      return prevState - 1;
+    });
+  };
+
+  return (
+    <main className="py-5 bg-whitesmoke">
+      <div className="product-single">
+        <div className="container">
+          <div className="product-single-content bg-white grid">
+            <div className="product-single-l">
+              <div className="product-img">
+                <div className="product-img-zoom">
+                  <img
+                    src={
+                      product ? (product.images ? product.images[0] : "") : ""
+                    }
+                    alt=""
+                    className="img-cover"
+                  />
+                </div>
+
+                <div className="product-img-thumbs flex align-center my-2">
+                  <div className="thumb-item">
+                    <img
+                      src={
+                        product ? (product.images ? product.images[1] : "") : ""
+                      }
+                      alt=""
+                      className="img-cover"
+                    />
+                  </div>
+                  <div className="thumb-item">
+                    <img
+                      src={
+                        product ? (product.images ? product.images[2] : "") : ""
+                      }
+                      alt=""
+                      className="img-cover"
+                    />
+                  </div>
+                  <div className="thumb-item">
+                    <img
+                      src={
+                        product ? (product.images ? product.images[3] : "") : ""
+                      }
+                      alt=""
+                      className="img-cover"
+                    />
+                  </div>
+                  <div className="thumb-item">
+                    <img
+                      src={
+                        product ? (product.images ? product.images[4] : "") : ""
+                      }
+                      alt=""
+                      className="img-cover"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="product-single-r">
+              <div className="product-details font-manrope">
+                <div className="title fs-20 fw-5">{product?.title}</div>
+                <div>
+                  <p className="para fw-3 fs-15">{product?.description}</p>
+                </div>
+                <div className="info flex align-center flex-wrap fs-14">
+                  <div className="rating">
+                    <span className="text-orange fw-5">Rating:</span>
+                    <span className="mx-1">{product?.rating}</span>
+                  </div>
+                  <div className="vert-line"></div>
+                  <div className="brand">
+                    <span className="text-orange fw-5">Brand:</span>
+                    <span className="mx-1">{product?.brand}</span>
+                  </div>
+                  <div className="vert-line"></div>
+                  <div className="brand">
+                    <span className="text-orange fw-5">Category:</span>
+                    <span className="mx-1 text-capitalize">
+                      {product?.category
+                        ? product.category.replace("-", " ")
+                        : ""}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="price">
+                  <div className="flex align-center">
+                    <div className="old-price text-gray">
+                      {formatPrice(product?.price)}
+                    </div>
+                    <span className="fs-14 mx-2 text-dark">
+                      Inclusive of all taxes
+                    </span>
+                  </div>
+
+                  <div className="flex align-center my-1">
+                    <div className="new-price fw-5 font-poppins fs-24 text-orange">
+                      {formatPrice(discountedPrice)}
+                    </div>
+                    <div className="discount bg-orange fs-13 text-white fw-6 font-poppins">
+                      {product?.discountPercentage}% OFF
+                    </div>
+                  </div>
+                </div>
+
+                <div className="qty flex align-center my-4">
+                  <div className="qty-text">Quantity:</div>
+                  <div className="qty-change flex align-center mx-3">
+                    <button
+                      type="button"
+                      className="qty-decrease flex align-center justify-center"
+                      onClick={() => decreaseQty()}
+                    >
+                      <i className="fas fa-minus"></i>
+                    </button>
+                    <div className="qty-value flex align-center justify-center">
+                      {quantity}
+                    </div>
+                    <button
+                      type="button"
+                      className="qty-increase flex align-center justify-center"
+                      onClick={() => increaseQty()}
+                    >
+                      <i className="fas fa-plus"></i>
+                    </button>
+                  </div>
+                  {product?.stock === 0 ? (
+                    <div className="qty-error text-uppercase bg-danger text-white fs-12 ls-1 mx-2 fw-5">
+                      out of stock
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+
+                <div className="btns">
+                  <button type="button" className="add-to-cart-btn btn">
+                    <i className="fas fa-shopping-cart"></i>
+                    <span>
+                      add to cart
+                    </span>
+                  </button>
+                  <button type="button" className="buy-now btn mx-3">
+                    <span className="btn-text">buy now</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ProductSinglePage;
