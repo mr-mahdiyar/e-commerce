e461ee1d1c11c82455e91ef958f428c56cdba971
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-18 16:21:07 +0330

    localStorage was added to cartSLice.

--- src/app/cartSlice.js
+++ src/app/cartSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Logger } from "sass";
 
+const fetchFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem(cart));
+  } else {
+    return [];
+  }
+
+  const storeInLocalStorage = (data) => {
+    localStorage.setItem("data", JSON.stringify(data));
+  };
+};
 const initialState = {
-  carts: [],
+  carts: fetchFromLocalStorage(),
   itemsCount: 0,
   totalAmount: 0,
   isCartMessageOn: false,
@@ -13,27 +24,32 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-        const isItemInCart = state.carts.find(item => item.id === action.payload.id);
+      const isItemInCart = state.carts.find(
+        (item) => item.id === action.payload.id
+      );
 
-        if(isItemInCart) {
-            const tempCart = state.carts.map(item => {
-                if(item.id === action.payload.id) {
-                    console.log("helllllllo...")
-                    let tempQty = item.quantity + action.payload.quantity;
-                    let tempTotalPrice = tempQty * item.price;
+      if (isItemInCart) {
+        const tempCart = state.carts.map((item) => {
+          if (item.id === action.payload.id) {
+            let tempQty = item.quantity + action.payload.quantity;
+            let tempTotalPrice = tempQty * item.price;
 
-                    return {
-                        ...item, quantity: tempQty, totalPrice: tempTotalPrice
-                    }
-                } else {
-                    return item;
-                }
-            });
+            return {
+              ...item,
+              quantity: tempQty,
+              totalPrice: tempTotalPrice,
+            };
+          } else {
+            return item;
+          }
+        });
 
-            state.carts = tempCart;
-        } else {
-            state.carts.push(action.payload);
-        }
+        state.carts = tempCart;
+        storeInLocalStorage(state.carts);
+      } else {
+        state.carts.push(action.payload);
+        storeInLocalStorage(state.carts);
+      }
     },
   },
 });
